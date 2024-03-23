46eba274069514a00bbc9c6f079e08a9f68d2fc9
Feridali <ferid62@hotmail.com>
2024-03-23 10:34:13 +0100

    klar med serch comonent

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,43 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./search.module.css";
+
 const URL = "https://auctioneer.azurewebsites.net/auction/s8w";
 
 export default function Search() {
-  const [search, setSearch] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchData, setSearchData] = useState([]);
 
   useEffect(() => {
-    function fetchApi() {
-      const res = fetch(URL);
-      const data = res.json;
+    async function fetchApi() {
+      const res = await fetch(URL);
+      const data = await res.json();
       console.log(data);
+      setSearchData(data);
     }
+    fetchApi();
   }, []);
+
+  const handleSearch = () => {
+    const searchedItem = searchData.find(
+      (item) => item.AuctionID === parseInt(searchTerm)
+    );
+    if (searchedItem) {
+      console.log("Sökt objekt:", searchedItem);
+    } else {
+      console.log("Ingen matchning hittades för ID:", searchTerm);
+    }
+  };
+
   return (
     <div className={styles.searchContainer}>
       <input
         className={styles.input}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        value={searchTerm}
         type="text"
         placeholder="Search for Id..."
       />
-      <button>Fetch</button>
+      <button onClick={handleSearch}>Fetch</button>
     </div>
   );
 }
