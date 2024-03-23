39cd7743352e6f0b688f52ffc7d1b2bb2b84e278
Feridali <ferid62@hotmail.com>
2024-03-23 09:03:23 +0100

    testade om de gick att fetcha

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
 import styles from "./search.module.css";
+const URL = "https://auctioneer.azurewebsites.net/auction/s8w";
 
 export default function Search() {
   const [search, setSearch] = useState([]);
 
   useEffect(() => {
-    function fetchApi() {}
+    function fetchApi() {
+      const res = fetch(URL);
+      const data = res.json;
+      console.log(data);
+    }
   }, []);
   return (
     <div className={styles.searchContainer}>
