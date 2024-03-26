2dce195869db742a33d12f2faa2995ac01148317
Feridali <ferid62@hotmail.com>
2024-03-26 13:18:51 +0100

    ändringar i search

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css'
-import NavigationBar from './components/NavigationBar';
-import CreateAuction from './components/CreateAuction';
-import Auctions from './components/Auctions';
-import ClosedAuction from './components/ClosedAuction';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import NavigationBar from "./components/NavigationBar";
+import CreateAuction from "./components/CreateAuction";
+import Auctions from "./components/Auctions";
+import ClosedAuction from "./components/ClosedAuction";
 import Search from "./components/Search";
-import DeleteAuctionComponent from './components/DeleteAuctionComponent';
+import DeleteAuctionComponent from "./components/DeleteAuctionComponent";
 
 const App = () => {
-  
   return (
     <Router>
       <div>
@@ -19,11 +18,14 @@ const App = () => {
           <Route path="/CreateAuction" element={<CreateAuction />} />
           <Route path="/Auctions" element={<Auctions />} />
           <Route path="/ClosedAuction" element={<ClosedAuction />} />
-          <Route path="/DeleteAuctionComponent" element={<DeleteAuctionComponent />} />
+          <Route
+            path="/DeleteAuctionComponent"
+            element={<DeleteAuctionComponent />}
+          />
         </Routes>
       </div>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
